0bee81198193d86087bcf2bdf63f44620ce6cb70
dayvsonred <dayvson.red@gmail.com>
2026-04-03 17:36:17 -0300

    update produtos

--- site/src/app/app-routing.module.ts
+++ site/src/app/app-routing.module.ts
@@ -12,15 +12,6 @@ const appRoutes: Routes = [
     pathMatch: 'full'
   },
   {
-    path: 'auth',
-    loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule),
-  },
-  {
-    path: 'dashboard',
-    loadChildren: () => import('./features/dashboard/dashboard.module').then(m => m.DashboardModule),
-    canActivate: [AuthGuard]
-  },
-    {
     path: 'home',
     loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule),
   },
@@ -43,70 +34,6 @@ const appRoutes: Routes = [
   {
     path: 'order-confirmation',
     loadChildren: () => import('./features/order-confirmation/order-confirmation.module').then(m => m.OrderConfirmationModule),
-  },
-  {
-    path: 'donation',
-    loadChildren: () => import('./features/donation/donation.module').then(m => m.DonationModule),
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'customers',
-    loadChildren: () => import('./features/customers/customers.module').then(m => m.CustomersModule),
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'users',
-    loadChildren: () => import('./features/users/users.module').then(m => m.UsersModule),
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'account',
-    loadChildren: () => import('./features/account/account.module').then(m => m.AccountModule),
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'icons',
-    loadChildren: () => import('./features/icons/icons.module').then(m => m.IconsModule),
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'typography',
-    loadChildren: () => import('./features/typography/typography.module').then(m => m.TypographyModule),
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'about',
-    loadChildren: () => import('./features/about/about.module').then(m => m.AboutModule),
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'pg/:id',
-    loadChildren: () => import('./features/campaign/campaign.module').then(m => m.CampaignModule),
-  },
-  {
-    path: 's2/:id',
-    redirectTo: 'pg/:id',
-    pathMatch: 'full'
-  },
-  {
-    path: 'donate',
-    loadChildren: () => import('./features/donate/donate.module').then(m => m.DonateModule),
-  },
-  {
-    path: 'architect',
-    loadChildren: () => import('./pages/architect-showcase/architect-showcase.module').then(m => m.ArchitectShowcaseModule),
-  },
-  {
-    path: 'portfolio-arquitetura',
-    loadChildren: () => import('./pages/architect-showcase/architect-showcase.module').then(m => m.ArchitectShowcaseModule),
-  },
-  {
-    path: 'Cloud-Engineer-Portfolio',
-    loadChildren: () => import('./pages/architect-showcase/architect-showcase.module').then(m => m.ArchitectShowcaseModule),
-  },
-  {
-    path: 'create',
-    loadChildren: () => import('./features/campaign/campaign.module').then(m => m.CampaignModule),
   },
   {
     path: '**',
